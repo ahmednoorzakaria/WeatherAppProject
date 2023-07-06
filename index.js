839e990f1a6966b920f752e6d1afe497b037a5a2
ahmednoorzakaria <ahmednoor.zakaria@student.moringaschool.com>
2023-07-06 08:55:41 +0300

    added background image and updated the colors

--- index.js
+++ index.js
@@ -11,24 +11,30 @@ document.addEventListener("DOMContentLoaded", () => {
     const data = await response.json();
 
 
-      document.querySelector(".city").innerHTML = data.name;
-      document.querySelector(".temp").innerHTML = data.main.temp + "°C";
-      document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-      document.querySelector(".wind").innerHTML = data.wind.speed + "Km/h";
+    if(response.status == 404){
+        document.querySelector('.error').style.display = 'block';
+        document.querySelector('.weather').style.display = 'none';
 
-      if (data.weather[0].main === "Clouds") {
-        weatherIcon.src = "./images/clouds.png";
-      } else if (data.weather[0].main === "Clear") {
-        weatherIcon.src = "./images/clear.png";
-      } else if (data.weather[0].main === "Rain") {
-        weatherIcon.src = "./images/rain.png";
-      } else if (data.weather[0].main === "Drizzle") {
-        weatherIcon.src = "./images/drizzles.png";
-      } else if (data.weather[0].main === "Mist") {
-        weatherIcon.src = "./images/mist.png";
-      }
+    }else{
+        console.log(data);
+    document.querySelector('.city').innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = data.main.temp + '°C';
+    document.querySelector('.humidity').innerHTML = data.main.humidity + '%';
+    document.querySelector('.wind').innerHTML = data.wind.speed + 'Km/h';
 
-      document.querySelector(".weather").style.display = "block";
+    if(data.weather[0].main == 'cloud'){
+        weatherIcon.src="./weather-app-img/images/clouds.png"
+    }else if(data.weather[0].main == 'clear'){
+        weatherIcon.src="./weather-app-img/images/clear.png"
+    }else if(data.weather[0].main == 'Rain'){
+        weatherIcon.src="./weather-app-img/images/rain.png"
+    }else if(data.weather[0].main == 'Drizzle'){
+        weatherIcon.src="./weather-app-img/images/drizzle.png"
+    }else if(data.weather[0].main == 'Mist'){
+        weatherIcon.src="./weather-app-img/images/mist.png"
+    }
+    document.querySelector('.weather').style.display = 'block';
+    }
     
   }
 
